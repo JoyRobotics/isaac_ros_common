a71730054449b13bc94a4ae9e33e99ba0b9c516d
Alaeda <linjing_w123@163.com>
2024-09-23 16:06:33 +0800

    add log tmux

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -18,6 +18,34 @@ for file in /opt/deploy/diablo_robot/config/supervisor/*; do
 done
 sudo supervisord -c /etc/supervisor/supervisord.conf
 
+# start log tmux
+tmux new-session -d -s dia_log
+
+# 创建窗口和面板
+tmux split-window -h -t dia_log
+tmux split-window -v -t dia_log:0.1
+tmux split-window -v -t dia_log:0.0
+tmux split-window -v -t dia_log:0.0
+tmux split-window -v -t dia_log:0.2
+tmux split-window -v -t dia_log:0.4
+tmux split-window -v -t dia_log:0.6
+
+# 等待窗口和面板创建完成
+sleep 2
+
+# 发送命令到各个面板
+tmux send-keys -t dia_log:0.0 'tail -f /var/log/diablo_robot/1_diablo_ctrl_node.log' C-m
+tmux send-keys -t dia_log:0.1 'tail -f /var/log/diablo_robot/2_diablo_driver.log' C-m
+tmux send-keys -t dia_log:0.2 'tail -f /var/log/diablo_robot/3_diablo_nav2_bringup.log' C-mm
+tmux send-keys -t dia_log:0.3 'tail -f /var/log/diablo_robot/4_navigation_sub.log' C-m
+tmux send-keys -t dia_log:0.4 'tail -f /var/log/diablo_robot/5_motion_cmd.log' C-m
+tmux send-keys -t dia_log:0.5 'sudo supervisorctl' C-m
+tmux send-keys -t dia_log:0.6 'tail -f /var/log/diablo_robot/7_param_ctrl.log' C-m
+tmux send-keys -t dia_log:0.7 'tail -f /var/log/diablo_robot/8_diablo_nvblox.log' C-m
+
+# 附加到会话
+# tmux attach-session -t dia_log
+
 # Restart udev daemon
 sudo service udev restart
 
