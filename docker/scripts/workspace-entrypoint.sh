ebf190ed614c3426a127bc556e70dbe62b331285
Alaeda <linjing_w123@163.com>
2024-12-16 11:38:57 +0800

    remove tmux

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -24,34 +24,34 @@ source ~/.bashrc
 for file in /opt/deploy/$PROJECT/deploy_scripts/supervisor/*; do
   sudo ln -s "$file" /etc/supervisor/conf.d/
 done
-#sudo supervisord -c /etc/supervisor/supervisord.conf
+sudo supervisord -c /etc/supervisor/supervisord.conf
 
 # start log tmux
-tmux new-session -d -s joy_log
+# tmux new-session -d -s joy_log
 
 # 创建窗口和面板
-tmux split-window -h -t joy_log
-tmux split-window -v -t joy_log:0.1
-tmux split-window -v -t joy_log:0.0
-tmux split-window -v -t joy_log:0.0
-tmux split-window -v -t joy_log:0.2
-tmux split-window -v -t joy_log:0.4
-tmux split-window -v -t joy_log:0.6
+# tmux split-window -h -t joy_log
+# tmux split-window -v -t joy_log:0.1
+# tmux split-window -v -t joy_log:0.0
+# tmux split-window -v -t joy_log:0.0
+# tmux split-window -v -t joy_log:0.2
+# tmux split-window -v -t joy_log:0.4
+# tmux split-window -v -t joy_log:0.6
 # tmux new-window -t joy_log
 
 # 等待窗口和面板创建完成
-sleep 3
+# sleep 3
 
 # 发送命令到各个面板
-LOG_DIR=/var/log/$PROJECT
-tmux send-keys -t dia_log:0.0 'tail -f /var/log/joy_robot/1_navigation.log' C-m
-tmux send-keys -t dia_log:0.1 'tail -f /var/log/joy_robot/2_perceptor.log' C-m
-tmux send-keys -t dia_log:0.2 'tail -f /var/log/joy_robot/3_lidar_mapping.log' C-m
-tmux send-keys -t dia_log:0.3 'tail -f /var/log/joy_robot/4_teleop.log' C-m
-tmux send-keys -t dia_log:0.4 'tail -f /var/log/joy_robot/5_*.log' C-m
-tmux send-keys -t dia_log:0.5 'sudo supervisorctl' C-m
-tmux send-keys -t dia_log:0.6 'tail -f /var/log/joy_robot/7_*.log' C-m
-tmux send-keys -t dia_log:0.7 'tail -f /var/log/joy_robot/8_*.log' C-m
+# LOG_DIR=/var/log/$PROJECT
+# tmux send-keys -t dia_log:0.0 'tail -f /var/log/joy_robot/1_diablo_ctrl_node.log' C-m
+# tmux send-keys -t dia_log:0.1 'tail -f /var/log/joy_robot/2_navigation.log' C-m
+# tmux send-keys -t dia_log:0.2 'tail -f /var/log/joy_robot/3_perceptor.log' C-m
+# tmux send-keys -t dia_log:0.3 'tail -f /var/log/joy_robot/4_lidar_mapping.log' C-m
+# tmux send-keys -t dia_log:0.4 'tail -f /var/log/joy_robot/5_teleop.log' C-m
+# tmux send-keys -t dia_log:0.5 'sudo supervisorctl' C-m
+# tmux send-keys -t dia_log:0.6 'tail -f /var/log/joy_robot/7_*.log' C-m
+# tmux send-keys -t dia_log:0.7 'tail -f /var/log/joy_robot/8_*.log' C-m
 # tmux send-keys -t dia_log:1.0 'tail -f /var/log/joy_robot/9_*.log' C-m
 
 # 附加到会话
